d62fbb039ccffaeb4e9b720a5e4456819171c1cc
vicecarloans <huyst1997@icloud.com>
2018-12-05 17:42:08 -0500

    Add DB Query and Pattern

--- app/src/main/java/ca/ducnguyen/a101095506/restaurantguide/helpers/DatabaseHelper.java
+++ app/src/main/java/ca/ducnguyen/a101095506/restaurantguide/helpers/DatabaseHelper.java
@@ -1,43 +1,46 @@
-//package ca.ducnguyen.a101095506.restaurantguide.helpers;
-//
-//import android.content.ContentValues;
-//import android.database.Cursor;
-//import android.support.v7.app.AppCompatActivity;
-//import android.os.Bundle;
-//import android.content.Context;
-//import android.database.sqlite.SQLiteDatabase;
-//import android.database.sqlite.SQLiteOpenHelper;
-//
-//
-//public class DatabaseHelper extends SQLiteOpenHelper {
-//
-//    public static final String DATABASE_NAME = "Assignment2.db";
-//    public static final String TABLE_NAME = "speakers_table";
-//    public static final String COL_1 = "ID";
-//    public static final String COL_2 = "NAME";
-//    public static final String COL_3 = "AFFILIATION";
-//    public static final String COL_4 = "EMAIL";
-//    public static final String COL_5 = "BIO";
-//
-//    public DatabaseHelper(Context context) {
-//        super(context, DATABASE_NAME , null, 1);
-//
-//    }
-//
-//    @Override
-//    public void onCreate(SQLiteDatabase db) {
-//        db.execSQL("create table " + TABLE_NAME + " (ID INTEGER PRIMARY KEY AUTOINCREMENT, NAME TEXT, AFFILIATION TEXT, EMAIL VARCHAR, BIO TEXT)"
-//        );
-//
-//    }
-//
-//
-//    @Override
-//    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-//        db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME);
-//        onCreate(db);
-//    }
-//
+package ca.ducnguyen.a101095506.restaurantguide.helpers;
+
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+
+public class DatabaseHelper extends SQLiteOpenHelper {
+
+    public static final String DATABASE_NAME = "RestaurantApp.db";
+    public static final String TABLE_NAME = "restaurants_table";
+    public static final String COL_1 = "ID";
+    public static final String COL_2 = "NAME";
+    public static final String COL_3 = "ADDRESS";
+    public static final String COL_4 = "DESCRIPTION";
+    public static final String COL_5 = "PHONE";
+    public static final String COL_6 = "TAGS";
+
+    public DatabaseHelper(Context context) {
+        super(context, DATABASE_NAME , null, 1);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL("create table " + TABLE_NAME + " (ID TEXT PRIMARY KEY, NAME TEXT, ADDRESS TEXT, DESCRIPTION TEXT, PHONE VARCHAR, TAGS VARCHAR)"
+        );
+
+    }
+
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME);
+        onCreate(db);
+    }
+
+    public SQLiteDatabase getWritableDatabase(){
+        return this.getWritableDatabase();
+    }
 //    public boolean insertData(String name, String affiliation, String email, String bio){
 //        SQLiteDatabase db = this.getWritableDatabase();
 //        ContentValues contentValues = new ContentValues();
@@ -57,5 +60,5 @@
 //        Cursor results = db.rawQuery("SELECT * FROM " + TABLE_NAME,null);
 //        return results;
 //    }
-//}
-//
+}
+
